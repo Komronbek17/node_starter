998c5da23eee08fbf1bfc8ebd66d974547eb8c9e
Sultonov Komron <k.sultonov@usoft.uz>
2022-07-22 11:45:34 +0500

    Initial Commit

---
+++ controllers/employeesController.js
@@ -0,0 +1,75 @@
+const data = {
+    employees: require('../model/employees.json'),
+    setEmployees(updateEmployees) {
+        this.employees = updateEmployees
+    },
+    addEmployees(newEmployee) {
+        this.employees.push(newEmployee)
+    }
+}
+
+const getAllEmployees = (req, res) => {
+    res.json(data.employees)
+}
+
+const createNewEmployee = (req, res) => {
+    const lastId = data.employees[this.employees.length - 1].id + 1 || 1
+    const newEmployee = {
+        id: lastId,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname
+    }
+
+    if (!(newEmployee.firstname || newEmployee.lastname)) {
+        res.status(400).json({
+            "message": "First and Last names are required"
+        })
+    }
+
+    data.addEmployees(newEmployee)
+    res.status(201).json(data.employees)
+}
+
+const updateEmployee = (req, res) => {
+    const empIndex = data.employees.findIndex(emp => emp.id === parseInt(req.body.id))
+    if (empIndex === -1) {
+        res.status(400).json({
+            "message": `Employee ID ${req.body.id} not found`
+        })
+    } else {
+        data.employees[empIndex].firstname = req.body.firstname
+        data.employees[empIndex].lastname = req.body.lastname
+        res.json(data.employees)
+    }
+}
+
+const deleteEmployee = (req, res) => {
+    const empIndex = data.employees.findIndex(emp => emp.id === parseInt(req.body.id))
+    if (empIndex === -1) {
+        res.status(400).json({
+            "message": `Employee ID ${req.body.id} not found`
+        })
+    } else {
+        const {id: removingId} = data.employees[empIndex]
+        data.employees = data.employees.filter(emp => emp.id !== removingId)
+    }
+}
+
+const getEmployee = (req, res) => {
+    const empIndex = data.employees.findIndex(emp => emp.id === parseInt(req.body.id))
+    if (empIndex === -1) {
+        res.status(400).json({
+            "message": `Employee ID ${req.body.id} not found`
+        })
+    } else {
+        res.json(data.employees[empIndex])
+    }
+}
+
+module.exports = {
+    getAllEmployees,
+    createNewEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getEmployee
+}
--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+const {logger} = require('./middleware/logEvents')
+const {errorHandler} = require('./middleware/errorHandler')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+app.use(express.urlencoded({
+    extended: false
+}))
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
+app.use(express.json())
 
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+const staticFiles = express.static(path.join(__dirname, '/public'))
+app.use('/', staticFiles)
+app.use('/subdirectory', staticFiles)
+
+app.use('/', require('./routes/root'))
+app.use('/subdirectory', require('./routes/subdirectory'))
+app.use('/employees',require('./routes/api/employees'))
 
 // Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
@@ -43,8 +52,25 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
+/*
+app.get('/!*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
+*/
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({
+            error: '404 Not Found'
+        })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
